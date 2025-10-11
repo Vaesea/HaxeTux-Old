b6181b014a32a8d696cbe30456b6b5499003d5a5
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-10-11 15:05:47 +0100

    Animation stuff for Tux

--- source/Tux.hx
+++ source/Tux.hx
@@ -4,8 +4,9 @@ package;
 // https://www.youtube.com/watch?v=Qdq-vXt-NOE
 // https://www.youtube.com/watch?v=aQazVHDztsg and yes i know this is for godot but it was actually helpful for this
 
+// I added lots of comments just in case someone wanted to do make a mod of this recreation but doesn't know how to code.
+
 import flixel.FlxG;
-import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.util.FlxColor;
@@ -19,29 +20,54 @@ class Tux extends FlxSprite
     var runSpeed = 320;
 
     // Jump stuff and Gravity
-    var jumpHeight = 576;
-    var gravity = 800;
-    var decelerateOnJumpRelease = 0.5; // thanks godot tutorial that i used
+    var jumpHeight = 576; // Jump Height
+    var gravity = 800; // Gravity, I don't recommend changing this but you can if you want low gravity or high gravity.
+    var decelerateOnJumpRelease = 0.5; // thanks godot tutorial that i used. also dont change this
 
-    // Images
-    var smallImage = FlxAtlasFrames.fromSparrow("assets/images/characters/tux/smalltux.png", "assets/images/characters/tux/smalltux.xml");
-    var bigImage = FlxAtlasFrames.fromSparrow("assets/images/characters/tux/tux.png", "assets/images/characters/tux/tux.xml");
-    var fireImage = FlxAtlasFrames.fromSparrow("assets/images/characters/tux/firetux.png", "assets/images/characters/tux/firetux.xml");
-    
+    // Image, if replaced, make sure the replacement image has the same animations!
+    var tuxImage = FlxAtlasFrames.fromSparrow("assets/images/characters/tux/smalltux.png", "assets/images/characters/tux/smalltux.xml");
+
+    // You probably shouldn't change any of the below if you're making a mod.
     public function new(x:Float = 0, y:Float = 0)
     {
         super(x, y);
-        makeGraphic(32, 32, FlxColor.WHITE);
-        drag.x = 1200;
-        acceleration.y = gravity;
+        
+        drag.x = 2400; // Add Deceleration
+        acceleration.y = gravity; // Add Gravity
+
+        // XML Spritesheet stuff, thanks Friday Night Funkin'
+        frames = tuxImage;
+        animation.addByPrefix('stand', 'stand', 6, false);
+        animation.addByPrefix('walk', 'walk', 6, true);
+        animation.play('stand');
     }
 
 	override public function update(elapsed:Float)
 	{
         // these will have to be changed if options are added that would allow the player to change keybinds or whatever it's called
         // Left + A = Go Left, Right + D = Go Right, Control = Run, Down + S = Duck (NOT ADDED YET!!!), Space + Up + W = Jump
-        // movement stuff here because of stupid elapsed:float thing that's needed for variable jump height i think
-        if (FlxG.keys.anyPressed([CONTROL]))
+        // i was gonna use the official haxeflixel way of doing variable jumping (using elapsed:float stuff) so that's why movement stuff is here. might still use it if the godot tutorial method turns out to not work that well for this, so don't move any of this stuff yet!
+
+        // Animation Stuff
+        if (velocity.x == 0)
+        {
+            animation.play('stand');
+        }
+        if (velocity.x != 0 && touching != WALL)
+        {
+            animation.play('walk');
+        }
+        if (velocity.x > 0)
+        {
+            flipX = false;
+        }
+        else if (velocity.x < 0)
+        {
+            flipX = true;
+        }
+
+        // Stuff to move Tux
+        if (FlxG.keys.anyPressed([CONTROL])) // Running
         {
             speed = runSpeed;
         }
@@ -49,22 +75,23 @@ class Tux extends FlxSprite
         {
             speed = walkSpeed;
         }
-        if (FlxG.keys.anyPressed([LEFT, A]) && !FlxG.keys.anyPressed([RIGHT, D]))
+        if (FlxG.keys.anyPressed([LEFT, A]) && !FlxG.keys.anyPressed([RIGHT, D])) // Moving Left
         {
             velocity.x = -speed;
         }
-        if (FlxG.keys.anyPressed([RIGHT, D]) && !FlxG.keys.anyPressed([LEFT, A]))
+        if (FlxG.keys.anyPressed([RIGHT, D]) && !FlxG.keys.anyPressed([LEFT, A])) // Moving Right
         {
             velocity.x = speed;
         }
-        if (FlxG.keys.anyPressed([SPACE, UP, W]) && isTouching(FlxDirectionFlags.FLOOR))
+        if (FlxG.keys.anyPressed([SPACE, UP, W]) && isTouching(FlxDirectionFlags.FLOOR)) // Jumping
         {
             velocity.y = -jumpHeight;
         }
-        if (FlxG.keys.anyJustReleased([SPACE, UP, W]) && velocity.y < 0)
+        if (FlxG.keys.anyJustReleased([SPACE, UP, W]) && velocity.y < 0) // Variable Jump Height Stuff
         {
             velocity.y = decelerateOnJumpRelease;
         }
-		super.update(elapsed);
+
+		super.update(elapsed); // Put this after the movement code, should probably also be after everything else in update.
 	}
 }
